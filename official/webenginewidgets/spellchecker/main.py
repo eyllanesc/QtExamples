6d4a8c29c552fb971a8b7edb0d97447c7043a224
eyllanesc <e.yllanescucho@gmail.com>
2020-05-24 12:37:40 -0500

    fix typo

--- official/webenginewidgets/spellchecker/main.py
+++ official/webenginewidgets/spellchecker/main.py
@@ -4,7 +4,7 @@ from PyQt5.QtCore import QCoreApplication, Qt, QUrl
 from PyQt5.QtWidgets import QAction, QApplication
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-import pellchecker_rc  # noqa: F401
+import spellchecker_rc  # noqa: F401
 
 
 class WebView(QWebEngineView):
