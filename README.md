4ea682b24c28f4f51a30f1ab3db53b664f7952f6
eyllanesc <e.yllanescucho@gmail.com>
2020-06-04 18:42:09 -0500

    add webenginewidgets/notifications

--- README.md
+++ README.md
@@ -1,35 +1,35 @@
 # QtExamples
-Translations of the official Qt examples into PyQt5 (also PySide2) and more.
+Translations of the official Qt examples into [PyQt5](https://pypi.org/project/PyQt5/) (also [PySide2](https://pypi.org/project/PySide2/)) and more.
 
  [![GitHub](https://img.shields.io/github/license/eyllanesc/QtExamples.svg)](https://github.com/eyllanesc/QtExamples/blob/master/LICENSE) [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black) [![Donate](https://img.shields.io/badge/donate-PayPal-blue.svg?logo=paypal)](https://www.paypal.me/eyllanesc)[![BuyMeACoffe](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/eyllanesc)
 
-Examples
-=========
+## Official Examples
 
-* [Official](official)
-  * [dbus](official/dbus)
-    * [pingpong](official/dbus/pingpong)
-  * [multimedia](official/multimedia/)
-    * [audiodevices](official/multimedia/audiodevices)
-  * [network](official/network)
-    * [googlesuggest](official/network/googlesuggest)
-    * [loopback](official/network/loopback)
-  * [webengine](official/webengine)
-    * [recipebrowser](official/webengine/recipebrowser)
-  * [webenginewidgets](official/webenginewidgets)
-    * [contentmanipulation](official/webenginewidgets/contentmanipulation)
-    * [cookiebrowser](official/webenginewidgets/cookiebrowser)
-    * [html2pdf](official/webenginewidgets/html2pdf)
-    * [maps](official/webenginewidgets/maps)
-    * [markdowneditor](official/webenginewidgets/markdowneditor)
-    * [minimal](official/webenginewidgets/minimal)
-    * [printme](official/webenginewidgets/printme)
-    * [spellchecker](official/webenginewidgets/spellchecker)
-    * [webui](official/webenginewidgets/webui)
+* [dbus](official/dbus)
+  * [pingpong](official/dbus/pingpong)
+* [multimedia](official/multimedia/)
+  * [audiodevices](official/multimedia/audiodevices)
+* [network](official/network)
+  * [googlesuggest](official/network/googlesuggest)
+  * [loopback](official/network/loopback)
+* [webengine](official/webengine)
+  * [recipebrowser](official/webengine/recipebrowser)
+* [webenginewidgets](official/webenginewidgets)
+  * [contentmanipulation](official/webenginewidgets/contentmanipulation)
+  * [cookiebrowser](official/webenginewidgets/cookiebrowser)
+  * [html2pdf](official/webenginewidgets/html2pdf)
+  * [maps](official/webenginewidgets/maps)
+  * [markdowneditor](official/webenginewidgets/markdowneditor)
+  * [minimal](official/webenginewidgets/minimal)
+  * [notifications](official/webenginewidgets/notifications)
+  * [printme](official/webenginewidgets/printme)
+  * [spellchecker](official/webenginewidgets/spellchecker)
+  * [webui](official/webenginewidgets/webui)
 
-* [others](others)
-  * [gst_pipeline](others/gst_pipeline)
-  * [matplotlib integration](others/matplotlib)
+## Additional Examples
+
+* [gstreamer pipeline](others/gst_pipeline)
+* [matplotlib integration](others/matplotlib)
 
 ## Donate
 
---
+++ official/webenginewidgets/notifications/main.py
@@ -0,0 +1,144 @@
+from PyQt5.QtCore import pyqtSlot, QCoreApplication, QPoint, Qt, QTimer, QUrl
+from PyQt5.QtGui import QDesktopServices, QPixmap
+from PyQt5.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QLabel,
+    QPushButton,
+    QSizePolicy,
+    QSpacerItem,
+    QVBoxLayout,
+    QWidget,
+)
+from PyQt5.QtWebEngineWidgets import QWebEnginePage, QWebEngineView
+
+import sip
+
+import data_rc  # noqa: F401
+
+
+class WebEnginePage(QWebEnginePage):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.featurePermissionRequested.connect(self._handle_featurePermissionRequested)
+
+    def acceptNavigationRequest(
+        self,
+        url: QUrl,
+        navigation_type: QWebEnginePage.NavigationType,
+        is_main_frame: bool,
+    ) -> bool:
+        if url.scheme() != "https":
+            return True
+        QDesktopServices.openUrl(url)
+        return False
+
+    def _handle_featurePermissionRequested(self, origin, feature):
+        if feature != QWebEnginePage.Notifications:
+            return
+        self.setFeaturePermission(
+            origin, feature, QWebEnginePage.PermissionGrantedByUser
+        )
+
+
+class NotificationPopup(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.m_icon = QLabel()
+        self.m_title = QLabel()
+        self.m_message = QLabel()
+
+        self.notification = None
+
+        self.setWindowFlags(Qt.ToolTip)
+        rootLayout = QHBoxLayout(self)
+
+        rootLayout.addWidget(self.m_icon)
+
+        bodyLayout = QVBoxLayout()
+        rootLayout.addLayout(bodyLayout)
+
+        titleLayout = QHBoxLayout()
+        bodyLayout.addLayout(titleLayout)
+
+        titleLayout.addWidget(self.m_title)
+        titleLayout.addItem(QSpacerItem(0, 0, QSizePolicy.Expanding))
+
+        close = QPushButton(self.tr("Close"))
+        titleLayout.addWidget(close)
+        close.clicked.connect(self.onClosed)
+
+        bodyLayout.addWidget(self.m_message)
+        self.adjustSize()
+
+    def present(self, newNotification):
+        if self.notification:
+            self.notification.close()
+            sip.delete(self.notification)
+            self.notification = None
+
+        self.notification = newNotification
+
+        self.m_title.setText(f"<b>{self.notification.title()}</b>")
+        self.m_message.setText(self.notification.message())
+        self.m_icon.setPixmap(
+            QPixmap.fromImage(self.notification.icon()).scaledToHeight(
+                self.m_icon.height()
+            )
+        )
+
+        self.show()
+        self.notification.show()
+
+        self.notification.closed.connect(self.onClosed)
+        QTimer.singleShot(
+            10000, lambda: self.onClosed() if self.notification is not None else None
+        )
+
+        # position our popup in the right corner of its parent widget
+        self.move(
+            self.parentWidget().mapToGlobal(
+                self.parentWidget().rect().bottomRight()
+                - QPoint(self.width() + 10, self.height() + 10)
+            )
+        )
+
+    @pyqtSlot()
+    def onClosed(self):
+        self.hide()
+        self.notification.close()
+        self.notification = None
+
+    def mouseReleaseEvent(self, event):
+        super().mouseReleaseEvent(event)
+        if self.notification and event.button() == Qt.LeftButton:
+            self.notification.click()
+            self.onClosed()
+
+
+def main():
+    import sys
+
+    QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QCoreApplication.setOrganizationName("QtExamples")
+
+    app = QApplication(sys.argv)
+
+    view = QWebEngineView()
+
+    # set custom page to open all page's links for https scheme in system browser
+    view.setPage(WebEnginePage(view))
+
+    profile = view.page().profile()
+    popup = NotificationPopup(view)
+    profile.setNotificationPresenter(popup.present)
+
+    view.resize(640, 480)
+    view.show()
+    view.setUrl(QUrl("qrc:/index.html"))
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
