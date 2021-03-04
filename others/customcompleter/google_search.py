b24d59566534862f31191801fe363a47a3d5ac22
eyllanesc <e.yllanescucho@gmail.com>
2021-03-04 18:08:51 -0500

    customcompleter

---
+++ others/customcompleter/google_search.py
@@ -0,0 +1,51 @@
+import sys
+import xml.etree.ElementTree as ET
+
+from lib import SuggestionCompleter, SuggestionNetworkModel
+
+from PyQt5.QtCore import Qt, QUrl, QUrlQuery
+from PyQt5.QtNetwork import QNetworkRequest, QNetworkReply
+from PyQt5.QtWidgets import QApplication, QLineEdit
+
+
+class SearchSuggestionModel(SuggestionNetworkModel):
+    def create_request(self, text):
+        url = QUrl("http://toolbarqueries.google.com/complete/search")
+        query = QUrlQuery()
+        query.addQueryItem("q", text)
+        query.addQueryItem("output", "toolbar")
+        query.addQueryItem("hl", "en")
+        url.setQuery(query)
+        request = QNetworkRequest(url)
+        return request
+
+    def process_reply(self, reply):
+        results = []
+        error_string = ""
+        if reply.error() == QNetworkReply.NoError:
+            content = reply.readAll().data()
+            suggestions = ET.fromstring(content)
+            for data in suggestions.iter("suggestion"):
+                results.append(data.attrib["data"])
+        elif reply.error() != QNetworkReply.OperationCanceledError:
+            error_string = reply.errorString()
+        return results, error_string
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    searchbar = QLineEdit()
+    completer = SuggestionCompleter(caseSensitivity=Qt.CaseInsensitive)
+    model = SearchSuggestionModel()
+    completer.setModel(model)
+    searchbar.setCompleter(completer)
+    searchbar.resize(640, 60)
+    searchbar.show()
+
+    ret = app.exec_()
+    sys.exit(ret)
+
+
+if __name__ == "__main__":
+    main()
---
+++ others/customcompleter/lib.py
@@ -0,0 +1,74 @@
+from collections.abc import Iterable
+from functools import cached_property
+from typing import List, Optional, Tuple
+
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QEventLoop, QObject
+from PyQt5.QtGui import QStandardItem, QStandardItemModel
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
+from PyQt5.QtWidgets import QCompleter
+
+
+class SuggestionNetworkModel(QStandardItemModel):
+    finished = pyqtSignal()
+    error = pyqtSignal(str)
+
+    def __init__(self, parent: Optional[QObject] = None):
+        super().__init__(parent)
+        self._current_reply: Optional[QNetworkReply] = None
+        self._error_string: str = ""
+
+    @cached_property
+    def network_manager(self) -> QNetworkAccessManager:
+        return QNetworkAccessManager()
+
+    @property
+    def error_string(self) -> str:
+        return self._error_string
+
+    @pyqtSlot(str)
+    def search(self, text: str) -> None:
+        self.clear()
+        if self._current_reply is not None:
+            self._current_reply.abort()
+        if text:
+            r = self.create_request(text)
+            self._current_reply = self.network_manager.get(r)
+            self._current_reply.finished.connect(self.handle_finished)
+        loop = QEventLoop()
+        self.finished.connect(loop.quit)
+        loop.exec_()
+
+    def create_request(self, text: str) -> QNetworkRequest:
+        request = QNetworkRequest()
+        return request
+
+    @pyqtSlot()
+    def handle_finished(self) -> None:
+        reply: Optional[QNetworkReply] = self.sender()
+        if reply is None:
+            return
+        if reply.error() == QNetworkReply.OperationCanceledError:
+            return
+        results, error_string = self.process_reply(reply)
+        if isinstance(results, Iterable):
+            for result in results:
+                item = QStandardItem(result)
+                self.appendRow(item)
+        if isinstance(error_string, str) and error_string:
+            self._error_string = error_string
+        else:
+            self._error_string = ""
+        self._current_reply = None
+        self.finished.emit()
+        reply.deleteLater()
+
+    def process_reply(self, reply: QNetworkReply) -> Tuple[List[str], str]:
+        return [], reply.errorString()
+
+
+class SuggestionCompleter(QCompleter):
+    def splitPath(self, path: str) -> List[str]:
+        if not isinstance(self.model(), SuggestionNetworkModel):
+            raise TypeError(f"error {SuggestionNetworkModel}")
+        self.model().search(path.strip())
+        return super().splitPath(path)
